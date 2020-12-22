48598526dda5eedc359f84f02a61de37fccec8a6
Yu Yasuda <38551932+YasudaYu@users.noreply.github.com>
2020-12-22 16:21:37 +0900

    完了したTodoを戻す機能の実装

--- src/index.js
+++ src/index.js
@@ -1,40 +1,72 @@
 import "./styles.css";
 
+//オンクリック時に実行される関数
 const onClickAdd = () => {
+  const inputText = getInputText();
+  addToIncompleteList(inputText);
+};
+//追加ボタンにオンクリックイベントを付与
+const addBtn = document.querySelector("#add-btn");
+addBtn.addEventListener("click", onClickAdd);
+
+//インプット値の取得
+const getInputText = () => {
   const input = document.querySelector("#add-text");
   const inputText = input.value; //textboxの値の取得
   // console.log(inputText);
   input.value = ""; //textboxの値の初期化
+  console.log(inputText);
+  return inputText;
+};
 
+const removeFromList = (target, listName) => {
+  const parent = target.parentNode;
+  document.querySelector(listName).removeChild(parent);
+};
+
+const addToCompleteList = (inputText) => {
+  const listName = "#complete-list";
+  // btn生成
+  const revbtn = document.createElement("button");
+  revbtn.innerText = "戻す";
+  revbtn.addEventListener("click", () => {
+    removeFromList(revbtn, listName);
+    addToIncompleteList(inputText);
+  });
+  const btns = [revbtn];
+  console.log(btns);
+  createListElement(inputText, btns, listName);
+};
+
+const addToIncompleteList = (inputText) => {
+  const listName = "#incomplete-list";
+  const cmpbtn = document.createElement("button");
+  cmpbtn.innerText = "完了";
+  cmpbtn.addEventListener("click", () => {
+    removeFromList(cmpbtn, listName); //完了ボタンの親タグを未完了TODOから削除
+    addToCompleteList(inputText); //完了したTODOにタスクを追加
+  });
+  const delbtn = document.createElement("button");
+  delbtn.innerText = "削除";
+  delbtn.addEventListener("click", () => {
+    removeFromList(delbtn, listName); //削除ボタンの親タグを未完了TODOから削除
+  });
+  const btns = [cmpbtn, delbtn];
+  console.log(btns);
+  createListElement(inputText, btns, listName);
+};
+
+const createListElement = (inputText, btns, listName) => {
   //div生成
   const div = document.createElement("div");
   div.className = "list-row";
   //li生成
   const li = document.createElement("li");
   li.innerText = inputText;
-  // btn生成
-  const cmpbtn = document.createElement("button");
-  cmpbtn.innerText = "完了";
-  cmpbtn.addEventListener("click", () => {
-    alert("完了");
-  });
-  const delbtn = document.createElement("button");
-  delbtn.innerText = "削除";
-  delbtn.addEventListener("click", () => {
-    //削除ボタンの親タグを未完了TODOから削除
-    const deleteTarget = delbtn.parentNode;
-    // console.log(deleteTarget);
-    document.querySelector("#incomplete-list").removeChild(deleteTarget);
-  });
-
-  //divタグの子要素に各要素を設定
   div.appendChild(li);
-  div.appendChild(cmpbtn);
-  div.appendChild(delbtn);
-
-  // 未完了リストに追加
-  document.querySelector("#incomplete-list").appendChild(div);
+  btns.map((btn) => div.appendChild(btn));
+  console.log(div);
+  console.log(listName);
+  document.querySelector(listName).appendChild(div);
+  console.log(document.querySelector(listName));
 };
-
-const addBtn = document.querySelector("#add-btn");
-addBtn.addEventListener("click", onClickAdd);
